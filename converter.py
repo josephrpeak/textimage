9c2c8dab1f492e74f1f08b53956cc4a88b178fd5
Joe <jpeak@aais.com>
2022-07-28 15:25:28 -0500

    added scaling, modified ascii table to include '.'

--- converter.py
+++ converter.py
@@ -7,8 +7,25 @@ ascii_grayscale = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!l
 
 outfile = open('output.txt', 'w')
 
-img = cv.imread('sponge.jpeg')
+img = cv.imread('images/astra.jpg')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+scale = 30
+
+if(scale != 100):
+	width = int(gray.shape[1]*scale / 100)
+	height = int(gray.shape[0]*scale / 100)
+	dim = (width, height)
+
+	gray = cv.resize(gray, dim)
+
+	rows = height
+	cols = width
+else:
+	rows = gray.shape[1]
+	cols = gray.shape[0]
+
+print(rows, cols)
 
 fig, ax = plt.subplots(1, 2, figsize=(16,8))
 fig.tight_layout()
@@ -19,27 +36,16 @@ ax[0].set_title("Original")
 ax[1].imshow(cv.cvtColor(gray, cv.COLOR_BGR2RGB))
 ax[1].set_title("Grayscale")
 
-rows = gray.shape[1]
-cols = gray.shape[0]
-
-print(rows, cols)
-
-asciiDict = {'1':'$','2':'@','3':'B','4':'%','5':'8','6':'&','7':'W','8':'M','9':'#','10':'*','11':'o','12':'a','13':'h','14':'k','15':'b','16':'d',
-			'17':'p','18':'q','19':'w','20':'m','21':'Z','22':'O','23':'0','24':'Q','25':'L','26':'C','27':'J','28':'U','29':'Y','30':'X','31':'z',
-			'32':'c','33':'v','34':'u','35':'n','36':'x','37':'r','38':'j','39':'f','40':'t','41':'/','42':'\\','43':'|','44':'(','45':')','46':'1',
-			'47':'{','48':'}','49':'[','50':']','51':'?','52':'-','53':'_','54':'+','55':'~','56':'<','57':'>','58':'i','59':'!','60':'l','61':'I',
-			'62':';','63':':','64':','}
-			
-		#print("Numerical Value: " + str(gray[j][i]))'''
+asciiDict = {'0':'$','1':'@','2':'B','3':'%','4':'8','5':'&','6':'W','7':'M','8':'#','9':'*','10':'o','11':'a','12':'h','13':'k','14':'b','15':'d','16':'p',
+			'17':'q','18':'w','19':'m','20':'Z','21':'O','22':'0','23':'Q','24':'L','25':'C','26':'J','27':'U','28':'Y','29':'X','30':'z','31':'c',
+			'32':'v','33':'u','34':'n','35':'x','36':'r','37':'j','38':'f','39':'t','40':'/','41':'\\','42':'|','43':'(','44':')','45':'1','46':'{',
+			'47':'}','48':'[','49':']','50':'?','51':'-','52':'_','53':'+','54':'~','55':'<','56':'>','57':'i','58':'!','59':'l','60':'I','61':';',
+			'62':':','63':',','64':'.'}
 
 for i in range(rows):
 	for j in range(cols):
-		if(gray[i][j] == 0):
-			gray[i][j] = 1
 		to_string = str(math.ceil((gray[i][j])/4))
 		outfile.write(asciiDict[to_string])
-		#outfile.write(to_string)
-		#print(gray[i][j])
 	outfile.write('\n')
 
 #plt.show()
